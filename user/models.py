460b9b900fb1d52db11d5075ee01e2ac0c04a291
HWISU96 <soshyzx123@naver.com>
2022-06-17 16:35:48 +0900

    [feat] user/models.py : AbstractBaseUser 상속받아서 USERNAME_FIELD 에 email 필드로 지정,

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
+class User(AbstractBaseUser):
+    username = models.CharField("사용자 계정", max_length=50, unique=True)
+    password = models.CharField("비밀번호", max_length=128)
+    email = models.EmailField("이메일 주소", max_length=100)
+    fullname = models.CharField("이름", max_length=20)
+    join_date = models.DateField("가입일", auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
